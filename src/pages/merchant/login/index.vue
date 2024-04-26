144f9eee3cc484eba5fd373c5a1c2d26c0075581
syx9527 <770460616@qq.com>
2024-04-26 10:43:57 +0800

    直观第一

--- src/pages/merchant/login/index.vue
+++ src/pages/merchant/login/index.vue
@@ -1,34 +1,55 @@
-<script lang="ts" setup>
-
-</script>
-
-<template>
-  <view tc>
-    <view mt-100rpx font-size-36rpx font-bold>
-      欢迎来到美预预约
-    </view>
-    <view mt-80rpx>
-      <view h-144rpx w-144rpx bg-blue ma />
-    </view>
-    <view mt-80rpx>
-      走近千家万户，共享轻松生活
-    </view>
-    <view mx-40rpx mt-112rpx color-white>
-      <wd-button size="large" custom-class="theme-bg" block>
-        <view flex flex-cc>
-          <span i-tdesign-logo-wechat font-size-36rpx />
-          <span>&nbsp;微信一键登录</span>
-        </view>
-      </wd-button>
-    </view>
-    <view mt120rpx>
-      切换为用户端登录
-    </view>
-  </view>
-</template>
-
 <route lang="yaml">
   layout: default
   style:
     navigationBarTitleText: 登录-商家端
 </route>
+
+<script lang="ts" setup>
+const checked = ref(false)
+function select(e: any) {
+  checked.value = !!e.detail.value.includes('cb')
+}
+</script>
+
+<template>
+  <div h-screen w-screen tc pr>
+    <div pt-100rpx font-size-36rpx font-bold>
+      欢迎来到美预预约
+    </div>
+    <div mt-80rpx>
+      <div h-144rpx w-144rpx bg-blue ma />
+    </div>
+    <div mt-80rpx>
+      走近千家万户，共享轻松生活
+    </div>
+    <div mx-40rpx mt-112rpx color-white>
+      <wd-button :disabled="!checked" size="large" custom-class="theme-bg" block>
+        <div flex flex-cc>
+          <span i-tdesign-logo-wechat font-size-36rpx />
+          <span>&nbsp;微信一键登录</span>
+        </div>
+      </wd-button>
+    </div>
+    <div mt120rpx>
+      切换为用户端登录
+    </div>
+    <div bottom-160rpx w-full color-gray-6 pf>
+      <div flex flex-cc>
+        <checkbox-group @change="select">
+          <checkbox
+            value="cb"
+            :checked="checked"
+          />
+        </checkbox-group>
+
+        <span>登录代表同意</span>
+        <span theme-color>《服务协议》</span>
+        <span>及</span>
+        <span theme-color>《隐私协议》</span>
+      </div>
+      <div pt-10rpx>
+        登录前请阅读
+      </div>
+    </div>
+  </div>
+</template>
