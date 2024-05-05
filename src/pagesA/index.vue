8ad477de71b85ee1798b4609c780069b011afa60
syx9527 <770460616@qq.com>
2024-05-06 04:39:36 +0800

    icon

--- src/pagesA/index.vue
+++ src/pagesA/index.vue
@@ -4,9 +4,6 @@
 <template>
   <div class="pagesA">
     这是分包A的内容
-
-    <span class="i-carbon-logo-github" />
-    <span class="i-mdi-light-home" text-size-5xl />
   </div>
 </template>
 
--- src/pagesB/index.vue
+++ src/pagesB/index.vue
@@ -4,7 +4,6 @@
 <template>
   <div class="pagesB">
     这是分包B的内容
-    <text i-logos-adobe-photoshop />
   </div>
 </template>
 
