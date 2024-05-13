0d3b82090c74e3188ef226778e1d2d0704541082
syx9527 <770460616@qq.com>
2024-05-13 16:11:13 +0800

    稍后添加

--- src/pages/business/init/steps/index.vue
+++ src/pages/business/init/steps/index.vue
@@ -8,56 +8,56 @@ type Status = 0 | 1 | 2 // 0 未添加 1 已添加 2 稍后添加
 interface Step {
   step: string
   name: string
+  desc: string
   icon: string
   color: string
   status: Status
-  count: number
   path: string
 }
 const arr = ref<Step[]>([
   {
     step: '一',
     name: '添加门店信息',
-    icon: 'i-ant-design-home-twotone',
-    color: 'color-#ff6619',
+    desc: '打造你的专属门店，方便客户找到你',
+    icon: 'i-material-symbols-edit-square',
+    color: 'color-#1a66ff',
     status: 0,
-    count: 0,
     path: '/pages/business/init/steps/step1',
   },
   {
     step: '二',
     name: '添加服务',
-    icon: 'i-tdesign-call',
+    desc: '创建你的第一个服务，方便客户预约',
+    icon: 'i-material-symbols-edit-square',
     color: 'color-#1a66ff',
     status: 1,
-    count: 1,
     path: '/pages/business/init/steps/step2',
   },
   {
     step: '三',
     name: '添加卡项',
-    icon: 'i-tdesign-cardmembership',
+    desc: '创建你的第一个会员卡：折扣卡、充值卡或者次卡',
+    icon: 'i-material-symbols-edit-square',
     color: 'color-#1a66ff',
     status: 2,
-    count: 0,
     path: '/pages/business/init/steps/step3',
   },
   {
     step: '四',
     name: '添加产品',
-    icon: 'i-tdesign-chart-3d',
+    desc: '创建你的第一个产品，方便客户购买开单',
+    icon: 'i-material-symbols-edit-square',
     color: 'color-#1a66ff',
     status: 1,
-    count: 0,
     path: '/pages/business/init/steps/step4',
   },
   {
     step: '五',
     name: '添加员工',
-    icon: 'i-tdesign-user',
+    desc: '创建你的第一个手艺人，方便客户预约时指定手艺人',
+    icon: 'i-material-symbols-edit-square',
     color: 'color-#1a66ff',
     status: 1,
-    count: 0,
     path: '/pages/business/init/steps/step5',
   },
 ])
@@ -89,26 +89,29 @@ const arr = ref<Step[]>([
       <view mb-16rpx color-999>
         第{{ item.step }}步
       </view>
-      <view flex bg-white p-40rpx flex-bt>
-        <view flex flex-cc>
-          <i mr-20rpx pr fs-40 :class="[item.icon, item.color]" />
-          <view fb>
-            {{ item.name }}
+      <view bg-white p-40rpx>
+        <view flex flex-bt>
+          <view flex flex-cc>
+            <i mr-20rpx pr fs-40 :class="[item.icon, item.color]" />
+            <view>
+              {{ item.name }}
+            </view>
+          </view>
+          <view flex tc flex-cc>
+            <view v-if="item.status === 0" pr-10rpx theme-color>
+              去添加
+            </view>
+            <view v-if="item.status === 1" flex pr-10rpx color-#2ecc71 flex-cc>
+              <i i-tdesign-check-circle-filled fs-50 />
+            </view>
+            <view v-if="item.status === 2" pr-10rpx color-#FA483C>
+              稍后添加
+            </view>
+            <i i-material-symbols-light-chevron-right fn fs-40 color-999 style="transform: translateY(2rpx);" />
           </view>
         </view>
-        <view flex tc flex-cc>
-          <view v-if="item.status === 0" pr-10rpx theme-color>
-            去添加
-          </view>
-          <view v-if="item.status === 1" flex pr-10rpx color-#2ecc71 flex-cc>
-            <text pr-50rpx fb color-333>
-              {{ item.count || '' }}
-            </text> <i i-tdesign-check-circle-filled fs-50 />
-          </view>
-          <view v-if="item.status === 2" pr-10rpx color-#FA483C>
-            稍后添加
-          </view>
-          <i i-material-symbols-light-chevron-right fn fs-40 color-999 style="transform: translateY(2rpx);" />
+        <view f12 color-999 pt-5px>
+          {{ item.desc }}
         </view>
       </view>
     </view>
