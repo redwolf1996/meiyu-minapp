41c998ffd0a67ce98bfb6154fdceffdf870b1a14
syx9527 <770460616@qq.com>
2024-05-13 15:21:14 +0800

    完善信息

--- src/App.vue
+++ src/App.vue
@@ -20,4 +20,23 @@ text {
 .custom-nav {
   background-color: #f6f6fd !important;
 }
+.wd-button.is-large {
+  border-radius: 0 !important;
+}
+.wd-button.is-disabled {
+  background: #d5d5d5 !important;
+}
+.wd-form {
+  .wd-input__inner {
+    text-align: right !important;
+  }
+  .wd-input__label {
+    padding-left: 5px !important;
+  }
+  .wd-input__label.is-required::after {
+    position: relative !important;
+    left: 4px !important;
+    top: 5px !important;
+  }
+}
 </style>
--- src/pages/business/login/info.vue
+++ src/pages/business/login/info.vue
@@ -1,19 +1,20 @@
 <route lang="yaml">
   style:
-    navigationBarTitleText: 完善信息
-  </route>
+    navigationBarTitleText: 完善登录信息
+</route>
 
 <script lang="ts" setup>
 const model = reactive<{
   value1: string
   value2: string
+  value3: string
 }>({
   value1: '',
   value2: '',
+  value3: '',
 })
 
 const form = ref()
-toast().error('校验通过')
 function handleSubmit() {
   form.value
     .validate()
@@ -22,7 +23,7 @@ function handleSubmit() {
         toast().success('校验通过')
     })
     .catch((error) => {
-      console.log(error, 'error')
+      toast().error(error)
     })
 }
 </script>
@@ -30,50 +31,53 @@ function handleSubmit() {
 <template>
   <view h-10px />
   <wd-form ref="form" :model="model">
-    <wd-cell-group border>
+    <wd-cell-group :border="true">
       <wd-input
         v-model="model.value1"
-        label="用户名"
-        label-width="100px"
+        label="姓名"
         prop="value1"
-        clearable
-        placeholder="请输入用户名"
+        placeholder="请输入"
+        suffix-icon="arrow-right"
         :rules="[{ required: true, message: '请填写用户名' }]"
       />
       <wd-input
         v-model="model.value2"
-        label="密码"
-        label-width="100px"
-        prop="value2"
-        show-password
-        clearable
-        placeholder="请输入密码"
-        :rules="[{ required: true, message: '请填写密码' }]"
+        label="手机号"
+        prop="value23232"
+        placeholder="请输入"
+        suffix-icon="arrow-right"
+        :rules="[{ required: true, message: '请填写手机号' }]"
       />
       <wd-input
         v-model="model.value2"
-        label="密码"
-        label-width="100px"
-        prop="value2"
-        show-password
-        clearable
-        placeholder="请输入密码"
-        :rules="[{ required: true, message: '请填写密码' }]"
-      />
-      <wd-input
-        v-model="model.value2"
-        label="密码"
-        label-width="100px"
-        prop="value2"
-        show-password
-        clearable
-        placeholder="请输入密码"
-        :rules="[{ required: true, message: '请填写密码' }]"
+        label="邀请码"
+        suffix-icon="arrow-right"
+        placeholder="请输入"
       />
     </wd-cell-group>
-    <view class="footer" p-24rpx>
-      <wd-button type="primary" size="large" block @click="handleSubmit">
-        提交
+    <view px-20px pt-80px>
+      <view f14 color-#F7CD24>
+        姓名填写须知
+      </view>
+      <view pt-10px f12 color-333>
+        <text fb>
+          姓名：
+        </text>如果您是服务者，请填写客户熟知的姓名，方便客户预约时指定您的服务
+      </view>
+      <view pt-10px f12 color-333>
+        <text fb>
+          手机号：
+        </text>请填写真实手机号，方便客户联系到你
+      </view>
+    </view>
+    <view p-20px>
+      <wd-button
+        :disabled="!model.value1 || !model.value2"
+        custom-class="theme-bg"
+        :round="false" type="primary" size="large" block
+        @click="handleSubmit"
+      >
+        保存
       </wd-button>
     </view>
   </wd-form>
