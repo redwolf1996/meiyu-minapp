92455a543820cf175ff95ea3ea166bc86512ee28
syx9527 <770460616@qq.com>
2024-05-22 14:42:15 +0800

    active

--- src/pages/business/init/steps/step3-1.vue
+++ src/pages/business/init/steps/step3-1.vue
@@ -5,7 +5,41 @@ style:
 </route>
 
 <script lang="ts" setup>
+const value = ref()
+const columns = ref(['选项1', '选项2', '选项3', '选项4', '选项5', '选项6', '选项7'])
+const form: any = reactive({
+  val: 1,
+})
+const model = reactive<{
+  value1: string
+  value2: string
+  value3: string
+}>({
+  value1: '',
+  value2: '',
+  value3: '',
+})
+const sources: any = [
+  {
+    label: '有限次卡',
+    value: 1,
+    isActive: true,
+  },
+  {
+    label: '不限次卡',
+    value: 2,
+    isActive: false,
+  },
+  {
+    label: '通卡',
+    value: 3,
+    isActive: false,
+  },
+]
 
+function test() {
+  console.log('xxx')
+}
 </script>
 
 <template>
@@ -17,33 +51,75 @@ style:
     也可跳过稍后在【工作台-产品管理】中添加
   </view>
 
-  <view m-32rpx bg-white px-32rpx py-60rpx>
-    <view h-44rpx flex>
-      <view hp100 style="width: 8rpx;background: #CABDFF;border-radius: 8rpx;" />
-      <view f18 fb style="padding-left: 8px;line-height: 44rpx;">
-        选择要填写的卡项类型
+  <wd-form ref="form" :model="model">
+    <view h-24rpx />
+    <view bg-white p-40rpx>
+      <view class="form-item-title required">
+        <text>选择次卡类型</text>
       </view>
+      <view h-28rpx />
+      <GridTagSelect v-model="form.val" :sources="sources" :columns="3" />
     </view>
-    <view h-30px />
-    <view flex flex-bt>
-      <wd-img
-        :width="80"
-        :height="80"
-        :src="`${IMG_BASE}/cika1.png`"
-      />
-      <wd-img
-        :width="80"
-        :height="80"
-        :src="`${IMG_BASE}/cika2.png`"
-      />
-      <wd-img
-        :width="80"
-        :height="80"
-        :src="`${IMG_BASE}/cika3.png`"
-      />
-    </view>
-  </view>
 
+    <view h-24rpx />
+    <wd-cell-group :border="true">
+      <wd-input
+        v-model="model.value1"
+        label="卡名称"
+        prop="value1"
+        placeholder="请输入"
+        suffix-icon="arrow-right"
+        :rules="[{ required: true, message: '请填写卡名称' }]"
+      />
+      <wd-picker
+        v-model="value"
+        :rules="[{ required: true, message: '请选择服务分类' }]"
+        label="卡分类"
+        align-right :columns="columns"
+      />
+      <wd-input
+        v-model="model.value2"
+        label="售价"
+        prop="value23232"
+        placeholder="请输入"
+        suffix-icon="arrow-right"
+        :rules="[{ required: true, message: '请填写售价' }]"
+      />
+    </wd-cell-group>
+
+    <view h-24rpx />
+    <wd-cell-group :border="true">
+      <wd-input
+        v-model="model.value1"
+        label="购卡权益"
+        disabled
+        prop="value1"
+        placeholder="请选择商品或服务"
+        suffix-icon="arrow-right"
+        :rules="[{ required: true, message: '请选择商品或服务' }]"
+        @click="test"
+      />
+      <view flex flex-bt flex-ac f13 style="padding:10px 20px;">
+        <view>产品名称1</view>
+        <view theme-red>
+          ¥499
+        </view>
+        <view>
+          <wd-input-number v-model="value" />
+        </view>
+      </view>
+      <view flex flex-bt flex-ac f13 style="padding:10px 20px;">
+        <view>产品名称1</view>
+        <view theme-red>
+          ¥499
+        </view>
+        <view>
+          <wd-input-number v-model="value" />
+        </view>
+      </view>
+      <view h-10rpx />
+    </wd-cell-group>
+  </wd-form>
   <view mx-40rpx mt-64rpx color-white>
     <wd-button size="large" custom-class="theme-bg" block>
       <view flex flex-cc>
@@ -63,4 +139,12 @@ style:
 </template>
 
 <style lang='scss' scoped>
+.end-time {
+  display: inline-block;
+  margin-left: 8px;
+  border: 1px solid #faa21e;
+  padding: 0 4px;
+  font-size: 10px;
+  color: #faa21e;
+}
 </style>
