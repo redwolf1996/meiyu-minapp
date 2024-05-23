2db8bad41cd0c100b21015b627b17a3ee8f215fd
syx9527 <770460616@qq.com>
2024-05-23 13:53:22 +0800

    z-page

--- src/pages/business/msg/list.vue
+++ src/pages/business/msg/list.vue
@@ -3,10 +3,35 @@ style:
   navigationBarTitleText: 消息
 </route>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const tab = ref<number>(0)
+const tabs = [{
+  label: '全部',
+  value: 1,
+}, {
+  label: '未读',
+  value: 2,
+}, {
+  label: '已读',
+  value: 3,
+}]
+</script>
 
 <template>
-  <view>测试</view>
+  <wd-tabs v-model="tab" :swipeable="true" :sticky="true" style="position: sticky;top: 0;">
+    <block v-for="item in tabs" :key="item">
+      <wd-tab :title="item.label" />
+    </block>
+  </wd-tabs>
+  <view class="content">
+    xxxx
+  </view>
 </template>
 
-<style lang='scss' scoped></style>
+<style lang='scss' scoped>
+.content {
+  line-height: 120px;
+  text-align: center;
+  height: 2000px;
+}
+</style>
