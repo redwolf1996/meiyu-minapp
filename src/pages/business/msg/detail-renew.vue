64392ed1286fbb86310eefcaa8df00025cd0dbf4
syx9527 <770460616@qq.com>
2024-05-24 08:40:35 +0800

    消息详情

--- src/pages/business/msg/detail-renew.vue
+++ src/pages/business/msg/detail-renew.vue
@@ -6,7 +6,27 @@ style:
 <script lang="ts" setup></script>
 
 <template>
-  <view>测试</view>
+  <view px-32rpx py-40rpx>
+    <view flex flex-bt flex-ac gap-14px>
+      <wd-img
+        :width="32"
+        :height="32"
+        :src="`${IMG_BASE}/icon-ring.png`"
+      />
+      <view flex-grow-1>
+        <view flex flex-bt flex-ac>
+          <view f16 fb>
+            续费成功
+          </view>
+        </view>
+        <view flex flex-bt tc flex-ac mt-10rpx>
+          <view f12 color-999>
+            2025.5.6 11:00:01
+          </view>
+        </view>
+      </view>
+    </view>
+  </view>
 </template>
 
 <style lang='scss' scoped></style>
