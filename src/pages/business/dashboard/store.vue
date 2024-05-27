4c1b3dd0d83c494fd52af774c0e47ac8cdecedc0
syx9527 <770460616@qq.com>
2024-05-27 16:38:17 +0800

    店务管理

--- src/pages/business/dashboard/store.vue
+++ src/pages/business/dashboard/store.vue
@@ -1,5 +1,5 @@
 <route lang="yaml">
-lay
+layout: pure
 style:
   navigationBarTitleText: 店务管理
 </route>
@@ -7,7 +7,105 @@ style:
 <script lang="ts" setup></script>
 
 <template>
-  <view>测试</view>
+  <view class="container">
+    <view p-30rpx bg-white rd-8px mb40rpx>
+      <view mb16px flex>
+        <view h-40rpx w-3.5px bg-#A7C4FD rd-8rpx />
+        <view f18 pl-10px lh-40rpx>
+          商品管理
+        </view>
+      </view>
+      <view flex flex-cc gap-100rpx tc>
+        <view>
+          <view>
+            <wd-img
+              :width="30"
+              :height="30"
+              :src="`${IMG_BASE}/dianwu/fuwu.png`"
+            />
+          </view>
+          <view f14 mt5px>
+            服务管理
+          </view>
+        </view>
+        <view>
+          <view>
+            <wd-img
+              :width="30"
+              :height="30"
+              :src="`${IMG_BASE}/dianwu/kaxiang.png`"
+            />
+          </view>
+          <view f14 mt5px>
+            卡项管理
+          </view>
+        </view>
+        <view>
+          <view>
+            <wd-img
+              :width="30"
+              :height="30"
+              :src="`${IMG_BASE}/dianwu/chanpin.png`"
+            />
+          </view>
+          <view f14 mt5px>
+            产品管理
+          </view>
+        </view>
+      </view>
+    </view>
+
+    <view p-30rpx bg-white rd-8px mb40rpx>
+      <view mb16px flex>
+        <view h-40rpx w-3.5px bg-#A7C4FD rd-8rpx />
+        <view f18 pl-10px lh-40rpx>
+          店铺管理
+        </view>
+      </view>
+      <view flex flex-cc gap-100rpx tc>
+        <view>
+          <view>
+            <wd-img
+              :width="30"
+              :height="30"
+              :src="`${IMG_BASE}/dianwu/yuangong.png`"
+            />
+          </view>
+          <view f14 mt5px>
+            员工管理
+          </view>
+        </view>
+        <view>
+          <view>
+            <wd-img
+              :width="30"
+              :height="30"
+              :src="`${IMG_BASE}/dianwu/yuyue.png`"
+            />
+          </view>
+          <view f14 mt5px>
+            预约管理
+          </view>
+        </view>
+        <view>
+          <view>
+            <wd-img
+              :width="30"
+              :height="30"
+              :src="`${IMG_BASE}/dianwu/zhifu.png`"
+            />
+          </view>
+          <view f14 mt5px>
+            支付管理
+          </view>
+        </view>
+      </view>
+    </view>
+  </view>
 </template>
 
-<style lang='scss' scoped></style>
+<style lang='scss' scoped>
+.container {
+  padding: 40rpx 30rpx;
+}
+</style>
