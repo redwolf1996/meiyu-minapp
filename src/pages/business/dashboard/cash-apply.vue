daeeada529cb232405388b4a37dd09212cf671f9
syx9527 <770460616@qq.com>
2024-06-08 15:04:48 +0800

    提现申请

--- src/pages/business/dashboard/cash-apply.vue
+++ src/pages/business/dashboard/cash-apply.vue
@@ -6,7 +6,44 @@ style:
 <script lang="ts" setup></script>
 
 <template>
-  <view>提现申请</view>
+  <view p-32rpx>
+    <view px-40rpx py-48rpx bg-blue h-340rpx color-#fff pr>
+      <view fs-36>
+        申请金额<span fs-28>（元）</span>
+      </view>
+      <view fs-64 fb tc abs-cc>
+        ¥9985.88
+      </view>
+    </view>
+
+    <view p-40rpx bg-white color-#888 fs-28 flex flex-ac gap-4px mt-32rpx>
+      <wd-icon name="warning" size="16px" />
+      <view>
+        <view lh-42rpx>
+          商家单日提现最高限额为2万元，最低为0.1元；
+        </view>
+        <view lh-42rpx>
+          商家一天只能提现1笔
+        </view>
+      </view>
+    </view>
+
+    <view mt-64rpx color-white>
+      <wd-button size="large" custom-class="theme-bg" block>
+        <view flex flex-cc>
+          <text>进入工作台</text>
+        </view>
+      </wd-button>
+    </view>
+    <view mx-40rpx color-white>
+      <wd-button size="large" type="text" block>
+        <view flex flex-cc color-#1A66FF>
+          <text>取消</text>
+        </view>
+      </wd-button>
+    </view>
+  </view>
 </template>
 
-<style lang='scss' scoped></style>
+<style lang='scss' scoped>
+</style>
