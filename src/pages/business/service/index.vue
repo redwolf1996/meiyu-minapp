5d40f033c1e3fccc79181677c77322f7503ce7c0
syx9527 <770460616@qq.com>
2024-06-14 17:39:29 +0800

    page11

--- src/pages/business/service/index.vue
+++ src/pages/business/service/index.vue
@@ -17,17 +17,93 @@ watch(() => searchText.value, (newVal) => {
     <view h-16px />
     <view mx-14px p-16px bg-white rd-8px>
       <view class="title">
-        <view>
-          <view class="border" />
-          <text>服务列表</text>
+        <view flex flex-ac>
+          <view class="bd-left" />
+          <div pl-10px>
+            服务列表
+          </div>
           <span f18 f14>
-            (共<span style="color:#1A66FF">10</span>项)
+            （共<span style="color:#1A66FF">10</span>项）
           </span>
         </view>
         <view class="plus">
-          <wd-icon name="add" size="14px" color="#1A66FF" />
+          <wd-img
+            :width="14"
+            :height="14"
+            :src="`${IMG_BASE}/icon-plus.png`"
+          />
         </view>
       </view>
+      <div mt-12px flex flex-bt flex-ac gap-40rpx pb-10px style="border-bottom: 1px solid #EFEFEF;">
+        <wd-img
+          :width="100"
+          :height="100"
+          mode="center"
+          :radius="12"
+          :src="`${IMG_BASE}/cat.png`"
+        />
+        <div flex flex-y flex-bt flex-1 h-108px>
+          <div flex flex-bt>
+            <div flex flex-ac gap-5px>
+              <wd-img
+                :width="16"
+                :height="16"
+                :src="`${IMG_BASE}/icon-stock.png`"
+              />
+              <span f12>80</span>
+            </div>
+            <wd-img
+              :width="20"
+              :height="20"
+              :src="`${IMG_BASE}/icon-more.png`"
+            />
+          </div>
+          <div fb f16>
+            幻彩悦光爽肤水
+          </div>
+          <div f12 color-#9A9FA5>
+            网店展示
+          </div>
+          <div>
+            <span fb f12 px-12rpx py-7rpx bg-#B5E4CA rd-12rpx>￥128</span>
+          </div>
+        </div>
+      </div>
+      <div mt-12px flex flex-bt flex-ac gap-40rpx pb-10px style="border-bottom: 1px solid #EFEFEF;">
+        <wd-img
+          :width="100"
+          :height="100"
+          mode="center"
+          :radius="12"
+          :src="`${IMG_BASE}/cat.png`"
+        />
+        <div flex flex-y flex-bt flex-1 h-108px>
+          <div flex flex-bt>
+            <div flex flex-ac gap-5px>
+              <wd-img
+                :width="16"
+                :height="16"
+                :src="`${IMG_BASE}/icon-time.png`"
+              />
+              <span f12>80分钟</span>
+            </div>
+            <wd-img
+              :width="20"
+              :height="20"
+              :src="`${IMG_BASE}/icon-more.png`"
+            />
+          </div>
+          <div fb f16>
+            幻彩悦光爽肤水
+          </div>
+          <div f12 color-#9A9FA5>
+            网店展示
+          </div>
+          <div>
+            <span fb f12 px-12rpx py-7rpx bg-#B5E4CA rd-12rpx>￥128</span>
+          </div>
+        </div>
+      </div>
     </view>
   </view>
 </template>
@@ -37,6 +113,12 @@ watch(() => searchText.value, (newVal) => {
   display: flex;
   justify-content: space-between;
   align-items: center;
+  .bd-left {
+    width: 3.5px;
+    height: 20px;
+    border-radius: 4px;
+    background: #a7c4fd;
+  }
 }
 .plus {
   width: 36px;
@@ -48,7 +130,7 @@ watch(() => searchText.value, (newVal) => {
   background: #fcfcfc;
   border-radius: 8px;
   box-shadow:
-    -1px -1px 8px rgba(0, 0, 0, 0.1),
-    4px 4px 8px rgba(0, 0, 0, 0.1);
+    -1px -1px 16px rgba(0, 0, 0, 0.08),
+    4px 4px 8px rgba(0, 0, 0, 0.08);
 }
 </style>
--- uno.config.ts
+++ uno.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
           'color',
           'size',
           'block',
+          'mode',
         ],
       },
     }),
