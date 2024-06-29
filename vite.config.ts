e1ec68adcb631eca2ac8c330b8735cbdf93c4c8c
syx9527 <770460616@qq.com>
2024-06-29 11:12:43 +0800

    类型提示

--- vite.config.ts
+++ vite.config.ts
@@ -1,61 +1,55 @@
 /// <reference types="vite/client" />
 
 import { defineConfig, loadEnv } from 'vite'
-import type { ConfigEnv } from 'vite'
 import Uni from '@dcloudio/vite-plugin-uni'
 import UniHelperManifest from '@uni-helper/vite-plugin-uni-manifest'
 import UniHelperPages from '@uni-helper/vite-plugin-uni-pages'
 import UniHelperLayouts from '@uni-helper/vite-plugin-uni-layouts'
 import UniHelperComponents from '@uni-helper/vite-plugin-uni-components'
 import AutoImport from 'unplugin-auto-import/vite'
-import { envParse, parseLoadedEnv } from 'vite-plugin-env-parse'
+import { envParse } from 'vite-plugin-env-parse'
 import UnoCSS from 'unocss/vite'
 
 // 注意插件的加载顺序，unihelper的相关插件要放在uni前面
-export default defineConfig(async ({ mode }) => {
-  const envInfo = parseLoadedEnv(loadEnv(mode, './'))
-  console.log(envInfo)
-  return {
-    plugins: [
-      envParse({ // https://github.com/yue1123/vite-plugin-env-parse
-        dtsPath: 'src/custom-env.d.ts',
-        build: true, // 小程序dev的默认行为本质是build, 所以打开这个选项以生成env正确的.d.ts文件
-      }),
-      UniHelperManifest(), // https://github.com/uni-helper/vite-plugin-uni-manifest
-      UniHelperPages({ // https://github.com/uni-helper/vite-plugin-uni-pages
-        dts: 'src/uni-pages.d.ts',
-        subPackages: ['src/pagesA', 'src/pagesB'],
-      }),
-      UniHelperLayouts(), // https://github.com/uni-helper/vite-plugin-uni-layouts
-      UniHelperComponents({ // https://github.com/uni-helper/vite-plugin-uni-components
-        dts: 'src/components.d.ts',
-        directoryAsNamespace: true,
-      }),
-      Uni(),
-      AutoImport({ // https://github.com/antfu/unplugin-auto-import
-        imports: [
-          'vue',
-          '@vueuse/core',
-          'uni-app',
-          {
-            'wot-design-uni': [
-              'useToast',
-            ],
-          },
-        ],
-        dts: 'src/auto-imports.d.ts',
-        dirs: ['src/composables', 'src/stores', 'src/utils', 'src/components/**/*'],
-        vueTemplate: true,
-      }),
-      UnoCSS(),
-    ],
-    build: {
-      sourcemap: true,
-    },
-    server: {
-      host: '0.0.0.0',
-      port: 7788,
-      https: false,
-    },
-  }
+export default defineConfig({
+  plugins: [
+    envParse({ // https://github.com/yue1123/vite-plugin-env-parse
+      dtsPath: 'src/custom-env.d.ts',
+      build: true, // 小程序dev的默认行为本质是build, 所以打开这个选项以生成env正确的.d.ts文件
+    }),
+    UniHelperManifest(), // https://github.com/uni-helper/vite-plugin-uni-manifest
+    UniHelperPages({ // https://github.com/uni-helper/vite-plugin-uni-pages
+      dts: 'src/uni-pages.d.ts',
+      subPackages: ['src/pagesA', 'src/pagesB'],
+    }),
+    UniHelperLayouts(), // https://github.com/uni-helper/vite-plugin-uni-layouts
+    UniHelperComponents({ // https://github.com/uni-helper/vite-plugin-uni-components
+      dts: 'src/components.d.ts',
+      directoryAsNamespace: true,
+    }),
+    Uni(),
+    AutoImport({ // https://github.com/antfu/unplugin-auto-import
+      imports: [
+        'vue',
+        '@vueuse/core',
+        'uni-app',
+        {
+          'wot-design-uni': [
+            'useToast',
+          ],
+        },
+      ],
+      dts: 'src/auto-imports.d.ts',
+      dirs: ['src/composables', 'src/stores', 'src/utils', 'src/components/**/*'],
+      vueTemplate: true,
+    }),
+    UnoCSS(),
+  ],
+  build: {
+    sourcemap: true,
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 7788,
+  },
 })
