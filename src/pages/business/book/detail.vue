698145b1f605b2bb442b045aa2b0e1cd6e1821a2
syx9527 <770460616@qq.com>
2024-07-16 09:10:40 +0800

    完成预约详情预约信息不分

--- src/pages/business/book/detail.vue
+++ src/pages/business/book/detail.vue
@@ -52,6 +52,58 @@ style:
         </view>
       </view>
     </view>
+    <view bg-white px-34rpx py-40rpx mt-16px>
+      <view flex flex-ac gap-20rpx>
+        <wd-img
+          :width="18"
+          :height="18"
+          :src="`${IMG_BASE}/icon-book.png`"
+        />
+        <text fs-34>
+          预约信息
+        </text>
+      </view>
+      <view mt-36rpx dib px-8px c-#00CCD8 bg-#CCF3F6 fs-22 h-20px lh-20px tc>
+        预约上门
+      </view>
+      <!-- <view mt-36rpx dib px-8px c-#FF5A5F bg-#FCE8E9 fs-22 h-20px lh-20px tc>
+        预约到店
+      </view> -->
+      <view mt-10px f12 c-#818181>
+        预约时间：<text c-#232220>
+          2022
+        </text>
+      </view>
+      <view mt-10px f12 c-#818181>
+        预约人：<text c-#232220>
+          2022
+        </text>
+      </view>
+      <view mt-10px f12 c-#818181>
+        手机号：<text c-#232220>
+          2022
+        </text>
+      </view>
+      <view mt-10px f12 c-#818181>
+        手艺人：<text c-#232220>
+          2022
+        </text>
+      </view>
+      <view mt-10px f12 c-#818181>
+        备注：<text c-#232220>
+          2022
+        </text>
+      </view>
+      <view mt-10px f12 c-#818181>
+        地址：<text c-#232220>
+          2022
+        </text>
+      </view>
+      <view style="height: 1px;background-color: #E6E6E6" wp-100 mt10px />
+      <view f14 tc mt-10px>
+        核销码&#12288;1780012312312
+      </view>
+    </view>
   </view>
 </template>
 
