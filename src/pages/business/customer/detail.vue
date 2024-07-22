85cecb1446014f32bbc9a822a9da17c09e9587e9
syx9527 <770460616@qq.com>
2024-07-23 07:59:52 +0800

    icon

--- src/pages/business/customer/detail.vue
+++ src/pages/business/customer/detail.vue
@@ -6,7 +6,61 @@ style:
 <script lang="ts" setup></script>
 
 <template>
-  <view>客户详情</view>
+  <view flex flex-ac p-40rpx gap-40rpx bg-white>
+    <wd-img
+      :round="true"
+      :width="64"
+      :height="64"
+      :src="`${IMG_BASE}/cat.png`"
+    />
+    <view h-136rpx flex-1 flex flex-bt flex-y>
+      <view f18>
+        李如霞
+      </view>
+      <view flex flex-bt flex-ac>
+        <view f12>
+          18777777777
+        </view>
+        <wd-img
+          :round="true"
+          :width="16"
+          :height="16"
+          :src="`${IMG_BASE}/icon-call.png`"
+        />
+      </view>
+      <view flex flex-ac gap-10rpx>
+        <wd-img
+          :round="true"
+          :width="16"
+          :height="16"
+          :src="`${IMG_BASE}/icon-v.png`"
+        />
+        <view class="tag">
+          永久有效
+        </view>
+      </view>
+    </view>
+  </view>
 </template>
 
-<style lang='scss' scoped></style>
+<style>
+page {
+  background-color: #fff;
+}
+</style>
+
+<style lang='scss' scoped>
+.tag {
+  color: #ff5a5f;
+  background: #feddde;
+  padding: 5rpx 16rpx;
+  height: 32rpx;
+  line-height: 32rpx;
+  text-align: center;
+  border-radius: 16rpx;
+  font-size: 20rpx;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
