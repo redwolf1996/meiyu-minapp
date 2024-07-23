252b09b58af26178fd7c4e97c8a858d9ac5fce35
syx9527 <770460616@qq.com>
2024-07-23 10:49:06 +0800

    完成预约档案

--- src/pages/business/book/list.vue
+++ src/pages/business/book/list.vue
@@ -1,28 +1,34 @@
 <script lang="ts" setup>
+const props = withDefaults(defineProps<{
+  showTabs?: boolean // 是否展示tabs
+}>(), {
+  showTabs: true,
+})
+
 const tab = ref<number>(0)
 const items = [{
   label: '待服务',
-  value: 1,
+  value: 0,
 }, {
   label: '服务中',
-  value: 2,
+  value: 1,
 }, {
   label: '已完成',
-  value: 3,
+  value: 2,
 }, {
   label: '全部',
-  value: 4,
+  value: 3,
 }]
 </script>
 
 <template>
-  <wd-tabs v-model="tab" :lineHeight="2" :lineWidth="24" color="#1A66FF">
+  <wd-tabs v-if="props.showTabs" v-model="tab" :lineHeight="2" :lineWidth="24" color="#1A66FF">
     <block v-for="item in items" :key="`t${item.value}`">
       <wd-tab :title="item.label" />
     </block>
   </wd-tabs>
   <view px-50rpx py-32rpx>
-    <view px-48rpx py-40rpx bg-white rd-10px>
+    <view px-48rpx py-40rpx bg-white rd-10px mb-32rpx>
       <view flex flex-ac flex-bt>
         <view flex flex-y gap-10px>
           <view c-#404143 f14 lh-14px>
@@ -103,7 +109,7 @@ const items = [{
         </view>
       </view>
 
-      <view flex flex-xr gap-16rpx mt-34rpx>
+      <view flex flex-xr mt-34rpx gap-14px>
         <button class="my-btn delete">
           删除
         </button>
@@ -113,9 +119,117 @@ const items = [{
         <button class="my-btn complete">
           完成
         </button>
-        <button class="my-btn complete">
+        <!-- <button class="my-btn complete">
           签到
+        </button> -->
+        <!-- <text class="my-status-tag to-service">
+          待服务
+        </text>
+        <text class="my-status-tag in-service">
+          服务中
+        </text>
+        <text class="my-status-tag end-service">
+          已完成
+        </text>
+        <text class="my-status-tag cancel-service">
+          已取消
+        </text> -->
+      </view>
+    </view>
+    <view px-48rpx py-40rpx bg-white rd-10px mb-32rpx>
+      <view flex flex-ac flex-bt>
+        <view flex flex-y gap-10px>
+          <view c-#404143 f14 lh-14px>
+            2023.11.22 8:00-10:00
+          </view>
+          <view f12 flex tc flex-ac gap-10rpx f10>
+            <view fb>
+              张硕
+            </view>
+            <view w-12rpx h-12rpx round style="background-color: #91919F;" />
+            <view color-white tc px-8rpx py-4rpx lh-24rpx bg-#FE502E>
+              到店服务
+            </view>
+          </view>
+        </view>
+        <view class="my-status-tag to-service">
+          待服务
+        </view>
+      </view>
+      <view h-32rpx />
+      <view>
+        <view flex gap-15px flex-ac mb-20rpx>
+          <wd-img
+            :width="44"
+            :height="44"
+            mode="aspectFill"
+            :src="`${IMG_BASE}/cat.png`"
+          />
+          <view flex-1 flex flex-y gap-20rpx>
+            <view flex flex-bt>
+              <text c-#0D0D26 f14 fb>
+                面部清洁补水
+              </text>
+              <text c-#3A3A3A f14>
+                x1
+              </text>
+            </view>
+            <view c-#161719 fs-20>
+              1小时
+            </view>
+          </view>
+        </view>
+        <view flex gap-15px flex-ac mb-20rpx>
+          <wd-img
+            :width="44"
+            :height="44"
+            mode="aspectFill"
+            :src="`${IMG_BASE}/cat.png`"
+          />
+          <view flex-1 flex flex-y gap-20rpx>
+            <view flex flex-bt>
+              <text c-#0D0D26 f14 fb>
+                面部清洁补水
+              </text>
+              <text c-#3A3A3A f14>
+                x1
+              </text>
+            </view>
+            <view c-#161719 fs-20>
+              1小时
+            </view>
+          </view>
+        </view>
+      </view>
+      <view flex flex-bt>
+        <view />
+        <view flex flex-ac gap-5px font-size-20rpx>
+          <wd-img
+            :width="20"
+            :height="20"
+            :src="`${IMG_BASE}/icon-people.png`"
+          />
+          <view fb>
+            王乐乐
+          </view>
+          <view w-10rpx h-10rpx round ma style="background-color: #000;" />
+          <view>13952768882</view>
+        </view>
+      </view>
+
+      <view flex flex-xr mt-34rpx gap-14px>
+        <button class="my-btn delete">
+          删除
         </button>
+        <button class="my-btn cancel">
+          取消
+        </button>
+        <button class="my-btn complete">
+          完成
+        </button>
+        <!-- <button class="my-btn complete">
+          签到
+        </button> -->
         <!-- <text class="my-status-tag to-service">
           待服务
         </text>
--- src/pages/business/customer/detail.vue
+++ src/pages/business/customer/detail.vue
@@ -3,53 +3,234 @@ style:
   navigationBarTitleText: 客户详情
 </route>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import List from '../book/List.vue';
+
+const tab = ref<number>(0)
+const tabs = [{
+  label: '订单记录',
+  value: 0,
+}, {
+  label: '预约记录',
+  value: 1,
+}, {
+  label: '会员档案',
+  value: 2,
+}]
+</script>
 
 <template>
-  <view flex flex-ac p-40rpx gap-40rpx bg-white>
-    <wd-img
-      :round="true"
-      :width="64"
-      :height="64"
-      :src="`${IMG_BASE}/cat.png`"
-    />
-    <view h-136rpx flex-1 flex flex-bt flex-y>
-      <view f18>
-        李如霞
-      </view>
-      <view flex flex-bt flex-ac>
-        <view f12>
-          18777777777
+  <view ps top-0 z-10000>
+    <view flex flex-ac p-40rpx gap-40rpx bg-white>
+      <wd-img
+        :round="true"
+        :width="64"
+        :height="64"
+        :src="`${IMG_BASE}/cat.png`"
+      />
+      <view h-136rpx flex-1 flex flex-bt flex-y>
+        <view f18>
+          李如霞
         </view>
-        <wd-img
-          :round="true"
-          :width="16"
-          :height="16"
-          :src="`${IMG_BASE}/icon-call.png`"
-        />
-      </view>
-      <view flex flex-ac gap-10rpx>
-        <wd-img
-          :round="true"
-          :width="16"
-          :height="16"
-          :src="`${IMG_BASE}/icon-v.png`"
-        />
-        <view class="tag">
-          永久有效
+        <view flex flex-bt flex-ac>
+          <view f12>
+            18777777777
+          </view>
+          <wd-img
+            :round="true"
+            :width="16"
+            :height="16"
+            :src="`${IMG_BASE}/icon-call.png`"
+          />
         </view>
+        <view flex flex-ac gap-10rpx>
+          <wd-img
+            :round="true"
+            :width="16"
+            :height="16"
+            :src="`${IMG_BASE}/icon-v.png`"
+          />
+          <view class="tag">
+            永久有效
+          </view>
+        </view>
+      </view>
+    </view>
+    <view grid grid-cols-3 p-40rpx bg-white f14>
+      <view>
+        <view c-#818181>
+          余额
+        </view>
+        <view mt-16rpx>
+          ￥1000.0
+        </view>
+      </view>
+      <view>
+        <view c-#818181 flex flex-ac>
+          会员卡
+          <wd-img
+            :width="16"
+            :height="16"
+            :src="`${IMG_BASE}/icon-arrow-right2.png`"
+          />
+        </view>
+        <view mt-16rpx>
+          2
+        </view>
+      </view>
+      <view>
+        <view c-#818181 flex flex-ac>
+          可用积分
+          <wd-img
+            :width="16"
+            :height="16"
+            :src="`${IMG_BASE}/icon-arrow-right2.png`"
+          />
+        </view>
+        <view mt-16rpx>
+          32
+        </view>
+      </view>
+    </view>
+    <view class="bs" />
+    <wd-tabs v-model="tab">
+      <block v-for="item in tabs" :key="item">
+        <wd-tab :title="item.label" />
+      </block>
+    </wd-tabs>
+  </view>
+
+  <!-- 内容区 -->
+  <view>
+    <!-- 订单记录 -->
+    <view v-if="tab === 0">
+      <view bg-white grid grid-cols-3 tc px-30px py-16px>
+        <view>
+          <view f14 mb10rpx>
+            ¥790
+          </view>
+          <view f13 c-#818181>
+            累计消费
+          </view>
+        </view>
+        <view>
+          <view f14 mb10rpx>
+            6
+          </view>
+          <view f13 c-#818181>
+            消费次数
+          </view>
+        </view>
+        <view>
+          <view f14 mb10rpx>
+            2024-05-13
+          </view>
+          <view f13 c-#818181>
+            上次消费
+          </view>
+        </view>
+      </view>
+      <view p32rpx>
+        <view px-16px py-20px bg-white rd-8px mb-12px>
+          <view flex flex-ac flex-bt>
+            <text>2024-04-20 17:34:20</text>
+            <text class="my-status-tag end-service">
+              已完成
+            </text>
+          </view>
+          <view flex flex-ac mt20px gap-8px pb-10px>
+            <wd-img
+              :width="40"
+              :height="40"
+              radius="8px"
+              :src="`${IMG_BASE}/cat.png`"
+            />
+            <view flex-1 flex flex-y flex-bt h-42px>
+              <view f14 flex flex-bt>
+                <view>面部清洁补水</view>
+                <view>￥98</view>
+              </view>
+              <view f12 c-#717171>
+                基础版 x1
+              </view>
+            </view>
+          </view>
+          <view h-1px bg-#EBEBF0 />
+          <view flex flex-ac flex-bt pt-12px>
+            <view>MY2024040910101000045</view>
+            <view c-#FF4070>
+              实付：￥98
+            </view>
+          </view>
+        </view>
+        <view px-16px py-20px bg-white rd-8px mb-12px>
+          <view flex flex-ac flex-bt>
+            <text>2024-04-20 17:34:20</text>
+            <text class="my-status-tag end-service">
+              已完成
+            </text>
+          </view>
+          <view flex flex-ac mt20px gap-8px pb-10px>
+            <wd-img
+              :width="40"
+              :height="40"
+              radius="8px"
+              :src="`${IMG_BASE}/cat.png`"
+            />
+            <view flex-1 flex flex-y flex-bt h-42px>
+              <view f14 flex flex-bt>
+                <view>面部清洁补水</view>
+                <view>￥98</view>
+              </view>
+              <view f12 c-#717171>
+                基础版 x1
+              </view>
+            </view>
+          </view>
+          <view h-1px bg-#EBEBF0 />
+          <view flex flex-ac flex-bt pt-12px>
+            <view>MY2024040910101000045</view>
+            <view c-#FF4070>
+              实付：￥98
+            </view>
+          </view>
+        </view>
+      </view>
+    </view>
+    <!-- 预约记录 -->
+    <view v-if="tab === 1">
+      <List :showTabs="false" />
+    </view>
+    <!-- 会员档案 -->
+    <view v-if="tab === 2">
+      22
+    </view>
+  </view>
+  <view h-100px />
+
+  <view pf bottom-0 wp100 style="border-top: 1px solid #eee">
+    <!-- <view h32rpx bg-#F5F5FA /> -->
+    <view p-40rpx grid grid-cols-3 grid-gap-10px bg-white>
+      <view class="my-btn theme-out rd0">
+        开卡充值
+      </view>
+      <view class="my-btn theme-out rd0">
+        开单
+      </view>
+      <view class="my-btn theme rd0">
+        预约
       </view>
     </view>
   </view>
 </template>
 
-<style>
-page {
-  background-color: #fff;
-}
-</style>
-
 <style lang='scss' scoped>
+.bs {
+  height: 1px;
+  box-shadow:
+    -1px -1px 16px rgba(0, 0, 0, 0.8),
+    4px 4px 8px rgba(0, 0, 0, 0.8);
+}
 .tag {
   color: #ff5a5f;
   background: #feddde;
