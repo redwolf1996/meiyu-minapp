20db0566a582f838b0a911acf4a7f17e0a3bfbc3
syx9527 <770460616@qq.com>
2024-07-24 09:12:01 +0800

    积分

--- src/pages/business/my/index.vue
+++ src/pages/business/my/index.vue
@@ -6,11 +6,49 @@ style:
 <script lang="ts" setup></script>
 
 <template>
-  <wd-navbar custom-class="custom-nav" :safeAreaInsetTop="true">
-    <template #title>
-      我的
-    </template>
-  </wd-navbar>
+  <view p20px bg-white>
+    <view flex flex-ac gap12px>
+      <wd-img
+        :round="true"
+        :width="56"
+        :height="56"
+        :src="`${IMG_BASE}/cat.png`"
+      />
+      <view flex-1 flex flex-y flex-bt h64px pr>
+        <view c-#141414 f18>
+          Hailey
+        </view>
+        <view flex flex-ac gap5px>
+          <text class="yy">
+            营业中
+          </text>
+          <text f14 c-#969699>
+            18888888888
+          </text>
+        </view>
+        <view c-#969699>
+          店铺拥有者
+        </view>
+        <view tc pa right-0 top-4px>
+          <view c-#969699 fs-20 mb2px>
+            积分
+          </view>
+          <view f18>
+            100
+          </view>
+        </view>
+      </view>
+    </view>
+  </view>
 </template>
 
-<style lang='scss' scoped></style>
+<style lang='scss' scoped>
+.yy {
+  padding: 2px 4px;
+  font-size: 11px;
+  line-height: 12px;
+  color: #fff;
+  background-color: #ff6619;
+  text-align: center;
+}
+</style>
