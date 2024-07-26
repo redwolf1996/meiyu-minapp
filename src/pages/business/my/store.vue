5f10bf34a6c86a62b498c4db6bf2689fbe58d9a6
syx9527 <770460616@qq.com>
2024-07-26 08:55:56 +0800

    门店简介

--- src/pages/business/my/store.vue
+++ src/pages/business/my/store.vue
@@ -1,12 +1,84 @@
 <route lang="yaml">
 style:
-  navigationBarTitleText: 店铺信息
+  navigationBarTitleText: 门店信息
 </route>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const value = ref()
+</script>
 
 <template>
-  <view>店铺信息</view>
+  <view
+    py-24rpx px-40rpx flex flex-bt flex-ac bg-white
+  >
+    <view c-#3B3D3D f14>
+      <view mb-2px>
+        员工头像
+      </view>
+      <view f12 c-#C9CDD4>
+        建议上传门店图片，方便客户了解你
+      </view>
+    </view>
+    <view flex flex-ac gap-6px>
+      <wd-upload :limit="1" action="https://ftf.jd.com/api/uploadImg" />
+      <wd-icon name="arrow-right" size="16px" color="#bfbfbf" />
+    </view>
+  </view>
+  <view h-12px />
+  <view mb12px>
+    <view c-#969699 f14 mb-12px pl20px>
+      门店名称
+    </view>
+    <view px40rpx py12px bg-white>
+      <wd-input v-model="value" :maxlength="20" show-word-limit :no-border="true" />
+    </view>
+  </view>
+  <view mb12px>
+    <view c-#969699 f14 mb-12px pl20px>
+      联系电话
+    </view>
+    <view px40rpx py12px bg-white>
+      <wd-input v-model="value" :maxlength="20" show-word-limit :no-border="true" />
+    </view>
+  </view>
+  <view mb12px>
+    <view c-#969699 f14 mb-12px pl20px>
+      所在地区
+    </view>
+    <view px40rpx py12px bg-white>
+      <wd-input v-model="value" :maxlength="20" show-word-limit :no-border="true" />
+    </view>
+  </view>
+  <view mb12px>
+    <view c-#969699 f14 mb-12px pl20px>
+      详细地址
+    </view>
+    <view px40rpx py12px bg-white>
+      <wd-input v-model="value" :maxlength="20" show-word-limit :no-border="true" />
+    </view>
+  </view>
+  <view mb12px>
+    <view c-#969699 f14 mb-12px pl20px>
+      门店简介
+    </view>
+    <view px40rpx py12px bg-white>
+      <wd-textarea v-model="value" :auto-height="true" placeholderStyle="font-size: 14px;color:#bfbfbf;" :maxlength="100" show-word-limit :no-border="true" />
+    </view>
+  </view>
+  <view h50px />
 </template>
 
-<style lang='scss' scoped></style>
+<style lang='scss' scoped>
+:deep(.wd-radio-group) {
+  line-height: 32 !important;
+}
+:deep(.wd-upload__evoke) {
+  margin-bottom: 0 !important;
+}
+:deep(.wd-upload__evoke-num) {
+  display: none;
+}
+:deep(.wd-upload__preview) {
+  margin: 0 0 0 0 !important;
+}
+</style>
