50f9864f895f1c2b9f4c8619a295c2544d5d11d9
syx9527 <770460616@qq.com>
2024-07-29 08:18:59 +0800

    完成添加预约页面

--- src/components/MyCell.vue
+++ src/components/MyCell.vue
@@ -2,9 +2,11 @@
 const props = withDefaults(defineProps<{
   label: string
   noBorder?: boolean
+  required?: boolean
 }>(), {
   label: '请输入标题',
   noBorder: false,
+  required: false,
 })
 
 const emits = defineEmits(['click'])
@@ -20,6 +22,9 @@ const emits = defineEmits(['click'])
     <view c-#3B3D3D fw-500 f14 flex flex-ac gap8px>
       <slot name="icon" />
       <text>{{ props.label }}</text>
+      <text v-if="props.required" class="required">
+        *
+      </text>
     </view>
     <view flex flex-ac gap-6px>
       <slot />
@@ -29,6 +34,14 @@ const emits = defineEmits(['click'])
 </template>
 
 <style scoped>
+.required {
+  font-size: var(--wot-cell-required-size, 18px);
+  line-height: 1.1;
+  color: var(--wot-cell-required-color, var(--wot-color-danger, #fa4350));
+  position: relative !important;
+  left: -4px !important;
+  top: 5px !important;
+}
 .cell {
   border-bottom: 1px solid #ebeef1;
 }
--- src/pages/business/book/add.vue
+++ src/pages/business/book/add.vue
@@ -3,10 +3,106 @@ style:
   navigationBarTitleText: 添加预约
 </route>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const form = ref()
+const value = ref()
+const columns = ref(['选项1', '选项2', '选项3', '选项4', '选项5', '选项6', '选项7'])
+const model = reactive<{
+  value1: string
+  value2: string
+  value3: string
+}>({
+  value1: '',
+  value2: '',
+  value3: '',
+})
+</script>
 
 <template>
-  <view>添加预约</view>
+  <wd-form ref="form" :model="model">
+    <wd-cell-group :border="true">
+      <wd-input
+        v-model="model.value1"
+        label="联系电话"
+        prop="value1"
+        placeholder="请输入"
+        suffix-icon="arrow-right"
+        :rules="[{ required: true, message: '请填写联系电话' }]"
+      />
+      <wd-input
+        v-model="model.value1"
+        label="联系人"
+        prop="value1"
+        placeholder="请输入"
+        suffix-icon="arrow-right"
+        :rules="[{ required: true, message: '请填写联系人' }]"
+      />
+      <wd-picker v-model="value" :rules="[{ required: true, message: '请选择服务方式' }]" label="服务方式" align-right :columns="columns" />
+      <wd-picker v-model="value" :rules="[{ required: true, message: '请选择服务时间' }]" label="服务时间" align-right :columns="columns" />
+      <wd-picker v-model="value" :rules="[{ required: true, message: '请选择手艺人' }]" label="手艺人" align-right :columns="columns" />
+    </wd-cell-group>
+  </wd-form>
+  <view>
+    <view flex flex-ac flex-bt f12 px20px py12px>
+      <view c-#3D3D3D>
+        预约服务
+      </view>
+      <view c-#1A66FF>
+        删除
+      </view>
+    </view>
+    <MyCellGroup>
+      <MyCell label="每天可服务时段" required noBorder>
+        <span f14 c-#B6BDBD pr4px>请选择</span>
+      </MyCell>
+      <view f12 c-#848486 mb10px flex flex-ac flex-bt>
+        <view>服务时长</view>
+        <view>约40分钟</view>
+      </view>
+      <view f12 c-#848486 flex flex-ac flex-bt>
+        <view>价格</view>
+        <view>￥100.00</view>
+      </view>
+      <view h14px />
+    </MyCellGroup>
+  </view>
+  <view>
+    <view flex flex-ac flex-bt f12 px20px py12px>
+      <view c-#3D3D3D>
+        预约服务
+      </view>
+      <view c-#1A66FF>
+        删除
+      </view>
+    </view>
+    <MyCellGroup>
+      <MyCell label="每天可服务时段" required noBorder>
+        <span f14 c-#B6BDBD pr4px>请选择</span>
+      </MyCell>
+      <view f12 c-#848486 mb10px flex flex-ac flex-bt>
+        <view>服务时长</view>
+        <view>约40分钟</view>
+      </view>
+      <view f12 c-#848486 flex flex-ac flex-bt>
+        <view>价格</view>
+        <view>￥100.00</view>
+      </view>
+      <view h14px />
+    </MyCellGroup>
+    <view style="border-top: 1px solid #EBEEF1" bg-white f14 c-#1A66FF tc h40px lh-40px>
+      +&nbsp;添加服务
+    </view>
+    <view px20px py12px>
+      备注
+    </view>
+    <view bg-white px-40rpx py-24rpx>
+      <wd-textarea
+        v-model="value"
+        placeholderStyle="font-size: 14px;color:#C9CDD4;"
+        placeholder="请输入预约备注" :maxlength="200" auto-height clearable show-word-limit
+      />
+    </view>
+  </view>
 </template>
 
 <style lang='scss' scoped></style>
