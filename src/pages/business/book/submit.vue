41b48e1d0c0b709f0084c0b379e11f6d8a73d056
syx9527 <770460616@qq.com>
2024-08-06 09:08:03 +0800

    预约人

--- src/pages/business/book/submit.vue
+++ src/pages/business/book/submit.vue
@@ -6,7 +6,45 @@ style:
 <script lang="ts" setup></script>
 
 <template>
-  <view>测试</view>
+  <view px15px py12px>
+    <view mb16px px12px py16px f12 flex flex-y gap-23rpx bg-white>
+      <view bg-#FCE8E9 c-#FF5A5F h38rpx w128rpx tc flex flex-cc>
+        预约到店
+      </view>
+      <view flex flex-ac>
+        <text c-#818181>
+          预约时间：
+        </text>
+        <text pl10px>
+          05-22 10:00
+        </text>
+      </view>
+      <view flex flex-ac>
+        <text c-#818181>
+          预约人：
+        </text>
+        <text pl10px>
+          Haily
+        </text>
+      </view>
+      <view flex flex-ac>
+        <text c-#818181>
+          手机号：
+        </text>
+        <text pl10px>
+          18988888888
+        </text>
+      </view>
+      <view flex flex-ac>
+        <text c-#818181>
+          手艺人：
+        </text>
+        <text pl10px>
+          到店分配
+        </text>
+      </view>
+    </view>
+  </view>
 </template>
 
 <style lang='scss' scoped></style>
