f7d9fe20c1a90476c47822533442029ccf3bdb74
syx9527 <770460616@qq.com>
2024-09-19 08:17:55 +0800

    tabs

--- src/pagesA/prod-servs.vue
+++ src/pagesA/prod-servs.vue
@@ -14,8 +14,70 @@ const items = [{
 }]
 const servs = ref([])
 const prods = ref([])
-
 const checked = ref(false)
+const active = ref<number>(1)
+const scrollTop = ref<number>(0)
+const subCategories = Array.from({ length: 24 }).fill({ title: '标题文字', label: '这是描述这是描述' }, 0, 24)
+const categories = ref([
+  {
+    label: '分类一',
+    title: '标题一',
+    icon: 'thumb-up',
+    items: subCategories,
+    disabled: false,
+  },
+  {
+    label: '分类二',
+    title: '标题二',
+    icon: 'thumb-up',
+    items: subCategories,
+    disabled: false,
+  },
+  {
+    label: '分类三',
+    title: '标题三',
+    icon: 'thumb-up',
+    items: subCategories.slice(0, 18),
+    disabled: false,
+  },
+  {
+    label: '分类四',
+    title: '标题四',
+    icon: 'thumb-up',
+    items: subCategories.slice(0, 21),
+    disabled: false,
+  },
+  {
+    label: '分类五',
+    title: '标题五',
+    icon: 'thumb-up',
+    items: subCategories,
+    disabled: false,
+  },
+  {
+    label: '分类六',
+    title: '标题六',
+    icon: 'thumb-up',
+    items: subCategories.slice(0, 18),
+    disabled: false,
+  },
+  {
+    label: '分类七',
+    title: '标题七',
+    icon: 'thumb-up',
+    items: subCategories,
+    disabled: true,
+  },
+])
+
+function handleChange({ value }) {
+  active.value = value
+  scrollTop.value = -1
+  nextTick(() => {
+    scrollTop.value = 0
+  })
+}
+
 function select(e: UniHelper.CheckboxGroupOnChangeEvent) {
   checked.value = !!e.detail.value.includes('cb')
 }
@@ -29,7 +91,39 @@ function toAdd() {}
       <wd-tab :title="item.label" />
     </block>
   </wd-tabs>
-  <view v-if="(tab === 0 && servs.length === 0) || (tab === 1 && prods.length === 0)" class="empty">
+
+  <view class="wraper">
+    <wd-sidebar v-model="active" @change="handleChange">
+      <wd-sidebar-item
+        v-for="(item, index) in categories"
+        :key="index"
+        :value="index"
+        :label="item.label"
+        :icon="item.icon"
+        :disabled="item.disabled"
+      />
+    </wd-sidebar>
+    <view class="content" :style="`transform: translateY(-${active * 100}%)`">
+      <scroll-view
+        v-for="(item, index) in categories"
+        :key="index"
+        class="category"
+        scroll-y
+        scroll-with-animation
+        :show-scrollbar="true"
+        :scroll-top="scrollTop"
+        :throttle="false"
+      >
+        <wd-cell-group :title="item.title" border>
+          <wd-cell v-for="(cell, index) in item.items" :key="index" :title="cell.title" :label="cell.label">
+            <wd-icon name="github-filled" size="24px" />
+          </wd-cell>
+        </wd-cell-group>
+      </scroll-view>
+    </view>
+  </view>
+
+  <!-- <view v-if="(tab === 0 && servs.length === 0) || (tab === 1 && prods.length === 0)" class="empty">
     <view tc mt56rpx f12 c-#979797>
       暂无数据
     </view>
@@ -42,7 +136,7 @@ function toAdd() {}
         </wd-button>
       </view>
     </view>
-  </view>
+  </view> -->
 
   <view class="footer">
     <view>
@@ -73,15 +167,36 @@ function toAdd() {}
 <style>
 page {
   height: 100vh;
+  background: #fff;
+  overflow-y: hidden;
 }
 </style>
 
 <style lang='scss' scoped>
+.wraper {
+  display: flex;
+  height: calc(100vh - var(--window-top) - 122px);
+  height: calc(100vh - var(--window-top) - 122px - constant(safe-area-inset-bottom));
+  height: calc(100vh - var(--window-top) - 122px - env(safe-area-inset-bottom));
+  overflow: hidden;
+}
+.content {
+  flex: 1;
+  background: #fff;
+  transition: transform 0.3s ease;
+  height: 100%;
+}
+.category {
+  box-sizing: border-box;
+  height: 100%;
+  overflow: hidden;
+}
 .footer {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 20rpx;
+  height: 80px;
+  padding: 0 20rpx;
   position: fixed;
   width: 100%;
   bottom: 0;
