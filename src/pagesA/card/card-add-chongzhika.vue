2a1494fc132801e6b8168eecb6ef023a298f395e
syx9527 <770460616@qq.com>
2024-10-18 15:40:48 +0800

    折扣卡 充值卡

--- src/pagesA/card/card-add-chongzhika.vue
+++ src/pagesA/card/card-add-chongzhika.vue
@@ -4,24 +4,37 @@ style:
 </route>
 
 <script lang="ts" setup>
-import piaoyiEditor from '@/uni_modules/piaoyi-editor/components/piaoyi-editor/piaoyi-editor.vue'
+import type { CardForm } from './types'
 
-const value = ref<number>(1)
-const columns = ref(['选项1', '选项2', '选项3', '选项4', '选项5', '选项6', '选项7'])
-const showDrawer = ref(false)
-const form: any = reactive({
-  val: 1,
+const userInfo = useUserStore()?.userInfo
+const storeInfo = userInfo?.storeList?.[0]
+const expiresType = ref(0) // 0不限次 1限次
+const form = ref<CardForm>({
+  storeId,
+  type: 1,
+  secondType: 1,
+  gift: 0,
+  name: '',
+  categoryId: computed(() => curClassify.value.id),
+  price: 0,
+  info: computed(() => {
+    const arr: any = [...checkedProds.value, ...checkedServs.value]
+    return arr.map((v) => {
+      return {
+        equity: 0,
+        productId: v.prodType === 1 ? v.id : null,
+        serviceId: v.prodType === 2 ? v.id : null,
+        name: v.name,
+        price: v.price,
+        price2: v.price2,
+      }
+    })
+  }),
+  expires: 0,
+  isShow: 1,
+  desc: computed(() => richData.value.content),
+  countLimit: 0,
 })
-const model = reactive<{
-  value1: string
-  value2: string
-  value3: string
-}>({
-  value1: '',
-  value2: '',
-  value3: '',
-})
-
 const sources2: any = [
   {
     label: '支持',
@@ -30,117 +43,104 @@ const sources2: any = [
   },
   {
     label: '不支持',
-    value: 2,
+    value: 0,
     isActive: false,
   },
 ]
 
-const readOnly = ref(false)
-const photoUrl = ref('http://test.com')
-const api = ref('api')
-const txt = ref('')
-const name = ref('file')
-const values = ref('')
+const catName = computed(() => curClassify.value.name)
 
-function setContents(e) {
-  txt.value = e.html
+function toCats() {
+  curClassify.value.type = CatType.Card
+  curClassify.value.multiple = false
+  uni.navigateTo({ url: '/pagesA/cats' })
 }
 
-function test() {
+function toProdServs() { // 商品和服务列表页面
+  uni.navigateTo({ url: '/pagesA/prod-servs' })
 }
 
-function handleClose() {}
+function toRichEdit() {
+  richData.value.key = 'card'
+  richData.value.title = '添加卡项说明'
+  uni.navigateTo({ url: '/pagesA/rich-edit' })
+}
+
+async function save() {
+  form.value.expires = expiresType.value ? form.value.expires : 0
+  await request.post<any>('/business/card', form.value)
+  await uni.showToast({ title: '创建成功' })
+  uni.navigateBack()
+}
+
+function changeEquity(val) {
+  console.log(val)
+}
 </script>
 
 <template>
-  <wd-form ref="form" :model="model">
+  <wd-form :model="form">
     <wd-cell-group :border="true">
       <wd-input
-        v-model="model.value1"
+        v-model="form.name"
         label="卡名称"
-        prop="value1"
         placeholder="请输入"
         suffix-icon="arrow-right"
-        :rules="[{ required: true, message: '请填写卡名称' }]"
       />
-      <wd-picker
-        v-model="value"
-        :rules="[{ required: true, message: '请选择服务分类' }]"
-        label="卡分类"
-        align-right :columns="columns"
-      />
+      <MyCellGroup>
+        <MyCell noBorder borderTop required label="卡分类" @myclick="toCats()">
+          <text v-if="!catName" f14 c-bfbfbf pr-5px>
+            请选择
+          </text>
+          <text v-else f14>
+            {{ catName }}
+          </text>
+        </MyCell>
+      </MyCellGroup>
       <wd-input
-        v-model="model.value2"
-        label="充值金额"
-        prop="value23232"
+        v-model="form.price"
+        label="售价"
+        type="number"
         placeholder="请输入"
         suffix-icon="arrow-right"
-        :rules="[{ required: true, message: '请填写充值金额' }]"
-      />
-      <wd-input
-        v-model="model.value2"
-        label="赠送金额"
-        prop="value23232"
-        placeholder="请输入"
-        suffix-icon="arrow-right"
+        :rules="[{ required: true, message: '请填写售价' }]"
       />
     </wd-cell-group>
 
     <view h-24rpx />
-    <wd-cell-group :border="true">
-      <wd-input
-        v-model="model.value1"
-        label="购卡权益"
-        disabled
-        prop="value1"
-        placeholder="请选择商品或服务"
-        suffix-icon="arrow-right"
-        :rules="[{ required: true, message: '请选择商品或服务' }]"
-        @click="test"
-      />
-      <view flex flex-bt flex-ac f13 h-96rpx px-40rpx>
-        <view wp-50>
-          产品名称1asdasdasdas
-        </view>
-        <view wp-10 theme-red>
-          ¥499
-        </view>
-        <view wp-40 flex flex-xr>
-          <view flex flex-ac>
-            <wd-input-number v-model="value" />
-            <text pl-10px>
-              折
+    <wd-cell-group :border="false">
+      <wd-cell title="购卡权益" is-link @click="toProdServs()">
+        <text c-#B6BDBD>
+          选择商品
+        </text>
+      </wd-cell>
+
+      <template v-if="form.info?.length">
+        <view v-for="(item, index) in form.info" :key="`ck-${index}`" flex flex-bt flex-ac f13 h-96rpx px20px>
+          <view>
+            <text>{{ item.name }}</text>
+            <text theme-red pl5px>
+              ¥{{ item.price2 }}
             </text>
           </view>
+          <!-- 有限次卡 -->
+          <wd-input-number v-show="form.secondType === 1" v-model="item.equity" @change="changeEquity" />
+          <!-- 不限次卡、通卡 -->
+          <text v-show="form.secondType === 2 || form.secondType === 3">
+            不限次
+          </text>
         </view>
-      </view>
-      <view flex flex-bt flex-ac f13 h-96rpx px-40rpx>
-        <view wp-50>
-          产品名asdas
-        </view>
-        <view wp-10 theme-red>
-          ¥499
-        </view>
-        <view wp-40 flex flex-xr>
-          <view flex flex-ac>
-            <wd-input-number v-model="value" />
-            <text pl-10px>
-              折
-            </text>
-          </view>
-        </view>
-      </view>
+      </template>
+
+      <!-- 通卡 -->
       <wd-input
-        v-if="form.val === 3"
-        v-model="model.value1"
-        label="可使用次数"
-        prop="value1"
+        v-if="form.secondType === 3"
+        v-model="form.countLimit"
+        label="已选服务共用次数"
+        type="number"
         placeholder="请输入"
         suffix-icon="arrow-right"
-        :rules="[{ required: true, message: '请输入可使用次数' }]"
-        @click="test"
       />
-      <view h-10rpx />
     </wd-cell-group>
 
     <view h-24rpx />
@@ -148,21 +148,55 @@ function handleClose() {}
       <view class="form-item-title required">
         <text>卡有效期</text>
       </view>
-      <wd-radio-group v-model="value" checkedColor="#fa4350">
-        <wd-radio value="1">
+      <wd-radio-group v-model="expiresType" checkedColor="#fa4350">
+        <wd-radio :value="0">
           永久有效
         </wd-radio>
-        <wd-radio value="2">
+        <wd-radio :value="1">
           <view flex flex-ac f14 style="color:rgba(0, 0, 0, 0.85)">
             <text>购买后</text>
             <input
-              px-10rpx w-46px tc theme-red type="number" placeholder="请输入"
-              placeholder-style="color:#b6bdbd;padding-top: 1px;"
+              v-model="form.expires"
+              px-10rpx w-42px tc theme-red type="number" placeholder="请输入"
+              placeholder-style="color:#b6bdbd;padding-top: 1px;font-size:14px;"
             >
             <text>天内有效</text>
           </view>
         </wd-radio>
       </wd-radio-group>
+    </view>
+
+    <view tc pr mt-28rpx>
+      <image
+        style="width: 630rpx;height: 368rpx;"
+        mode="aspectFit"
+        :src="`${IMG_BASE}/bg_zkk.png`"
+      />
+      <view
+        pa color-white
+        style="width: 630rpx;height: 368rpx;padding: 25px;
+                top: 0;left: 50%;text-align: left;
+                transform: translateX(-50%);"
+      >
+        <view flex flex-bt flex-ac>
+          <view f14 style="color: rgba(255, 255, 255, 0.7);">
+            硕园美甲美睫
+          </view>
+          <view text-20rpx py-8rpx px-20rpx tc style="background: #FF5F00;border-radius: 32rpx;">
+            折扣卡
+          </view>
+        </view>
+        <view text-48rpx pt-56rpx>
+          7980面部精雕30次
+        </view>
+        <view f14>
+          30次
+        </view>
+        <view f12 flex flex-bt pt-52rpx>
+          <view>购买后180天内有效</view>
+          <view>09/25&#12288;</view>
+        </view>
+      </view>
     </view>
 
     <view h-24rpx />
@@ -184,21 +218,20 @@ function handleClose() {}
         >
           <view flex flex-bt flex-ac>
             <view f14 style="color: rgba(255, 255, 255, 0.7);">
-              硕园美甲美睫
+              {{ storeInfo?.storeName || '--' }}
             </view>
-            <view text-20rpx py-8rpx px-20rpx tc style="background: #FF5F00;border-radius: 32rpx;">
-              充值卡
+            <view text-20rpx w-88rpx h-40rpx lh-40rpx tc style="background: #FF5F00;border-radius: 32rpx;">
+              折扣卡
             </view>
           </view>
           <view text-48rpx pt-56rpx>
-            7980面部精雕30次
+            {{ form.name }}
           </view>
           <view f14>
-            30次
+            充1000送200
           </view>
-          <view f12 flex flex-bt pt-52rpx>
-            <view>购买后180天内有效</view>
-            <view>09/25&#12288;</view>
+          <view v-if="form.expires !== null" f12 pt-52rpx>
+            {{ form.expires === 0 ? '永久有效' : `购买后${form.expires}天内有效` }}
           </view>
         </view>
       </view>
@@ -210,56 +243,26 @@ function handleClose() {}
         <text>网店售卖</text>
       </view>
       <view h-28rpx />
-      <GridTagSelect v-model="form.val" :sources="sources2" :columns="2" />
+      <GridTagSelect v-model="form.isShow" :sources="sources2" :columns="2" />
     </view>
 
     <view h-24rpx />
-    <view bg-white px-40rpx py-24rpx @click="showDrawer = true">
+    <view bg-white px-40rpx py-24rpx @click="toRichEdit()">
       <view mb-20rpx class="form-item-title">
         <text>卡项说明</text>
       </view>
-      <rich-text v-if="txt" :nodes="txt" />
+      <!-- 0是初始状态 11是清空后还包含空标签 <p><br></p>的字符数 -->
+      <rich-text v-if="richData.len !== 0 && richData.len !== 11" :nodes="richData.content" />
       <wd-textarea
         v-else
         readonly
         placeholderStyle="font-size: 14px;color:#C9CDD4;"
         placeholder="请输入卡项说明"
-        :maxlength="500" auto-height clearable show-word-limit
+        auto-height clearable show-word-limit
       />
     </view>
-    <wd-popup
-      v-model="showDrawer" :z-index="9999" position="bottom"
-      :closable="true" custom-style="width: 100%;height:90%"
-      @close="handleClose"
-    >
-      <view style="height: 100%;background-color: #fff;">
-        <view p-10px tc fb>
-          卡项说明
-        </view>
-        <view class="richtext">
-          <piaoyiEditor
-            fontsize="13px"
-            :values="values"
-            :maxlength="3000"
-            :readOnly="readOnly"
-            :photoUrl="photoUrl"
-            :api="api"
-            :name="name"
-            @changes="setContents"
-          />
-          <view class="">
-            {{ txt }}
-          </view>
-          <!-- <view flex flex-bt mt-20rpx>
-                    <wd-button @click="showDrawer = false">
-                      保存
-                    </wd-button>
-                  </view> -->
-        </view>
-      </view>
-    </wd-popup>
   </wd-form>
-  <view mx-40rpx mt-64rpx color-white>
+  <view mx-40rpx mt-64rpx color-white @click="save()">
     <wd-button size="large" custom-class="theme-bg" block>
       <view flex flex-cc>
         <text>保存</text>
@@ -273,11 +276,6 @@ function handleClose() {}
 .ipt {
   width: 100px !important;
 }
-// .wd-form {
-//   :deep(.wd-input__inner {
-//     text-align: left !important;
-//   }
-// }
 .end-time {
   display: inline-block;
   margin-left: 8px;
