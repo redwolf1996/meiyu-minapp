80ded4dae54aca169453de704f7017c6fb2b3ef5
syx9527 <770460616@qq.com>
2025-02-04 07:55:27 +0800

    注释

--- src/custom-env.d.ts
+++ src/custom-env.d.ts
@@ -5,9 +5,6 @@ interface ImportMetaEnv {
   readonly VITE_APP_URL_FWXY: string
   readonly VITE_APP_URL_YSZC: string
   readonly VITE_ENV: string
-  /**
-   * VITE_BASE_URL = https://api.test.kivibook.cn/kivi-beauty
-   */
   readonly VITE_BASE_URL: string
   readonly VITE_ENV_VERSION: string
   readonly VITE_CJS_IGNORE_WARNING: boolean
