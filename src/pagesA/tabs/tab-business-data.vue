831fe042234d397b1a9931ee6cad75ea717cff85
syx9527 <770460616@qq.com>
2025-05-09 15:09:51 +0800

    business data

--- src/pagesA/tabs/tab-business-data.vue
+++ src/pagesA/tabs/tab-business-data.vue
@@ -7,11 +7,26 @@ style:
 <script lang="ts" setup>
 import MyTabBar from './MyTabBar.vue'
 
-console.log('xxxxx')
+const tab = ref(0)
+const tabs = [{
+  label: '经营数据',
+}, {
+  label: '客户分析',
+}, {
+  label: '员工业绩',
+}, {
+  label: '员工统计',
+}]
 </script>
 
 <template>
   <MyNavBar title="数据" :capsule="false" />
+  <wd-tabs v-model="tab">
+    <block v-for="item in tabs" :key="item.label">
+      <wd-tab :title="item.label" />
+    </block>
+  </wd-tabs>
+
   <MyTabBar :tab-index="3" />
 </template>
 
