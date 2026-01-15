f6a287cc32bf519bd41006fa761550c8cfc5b3a0
syx9527 <syx9527@gmail.com>
2026-01-15 09:21:17 +0800

    长按识别二维码

--- src/pagesA/my/official-account.vue
+++ src/pagesA/my/official-account.vue
@@ -4,59 +4,46 @@ style:
 </route>
 
 <script lang="ts" setup>
-// official-account 组件只在以下场景显示：
-// 1. 扫描小程序码
-// 2. 搜索小程序
-// 3. 从其他小程序返回
-// 4. 从 App 分享消息卡片进入
-// 5. 小程序绑定的公众号文章内打开
+// 公众号二维码图片地址
+const qrcodeUrl = `${IMG_BASE}/qrcode-official.jpg`
 
-const showTip = ref(false)
-
-onLoad(() => {
-  // 检查是否支持显示公众号组件
-  // 如果不支持，显示二维码引导关注
-  showTip.value = true
-})
+// === 以下为备用方案（web-view 方式），保留不删除 ===
+// 直接使用公众号文章链接，用户可通过文章顶部进入公众号主页关注
+// const officialAccountUrl = 'https://mp.weixin.qq.com/s/HmYvUMUjoUWqOWs4lxHdlA'
 </script>
 
 <template>
   <view p20px>
-    <!-- 公众号关注组件，仅在特定场景下显示 -->
-    <!-- #ifdef MP-WEIXIN -->
-    <view mb20px>
-      <official-account />
-    </view>
-    <!-- #endif -->
-
     <view bg-white rd-12px p20px>
       <view tc mb20px>
+        <view f18 c-333 mb8px>
+          关注公众号
+        </view>
         <view f14 c-999>
-          关注后可接收预约提醒、消费通知等消息
+          长按下方二维码识别关注
         </view>
       </view>
 
-      <!-- 公众号二维码 -->
-      <view flex flex-cc mb20px>
-        <wd-img
-          :width="200"
-          :height="200"
-          :src="`${IMG_BASE}/qrcode-official.jpg`"
+      <!-- 长按识别二维码，show-menu-by-longpress 开启长按菜单 -->
+      <view flex flex-cc>
+        <image
+          :src="qrcodeUrl"
           mode="aspectFit"
+          show-menu-by-longpress
+          style="width: 400rpx; height: 400rpx;"
         />
       </view>
 
-      <view tc f14 c-666>
-        长按识别二维码关注
-      </view>
-    </view>
-
-    <view v-if="showTip" mt20px p16px bg-FFF9E6 rd-8px>
-      <view f14 c-FF9500 lh-22px>
-        提示：如果上方未显示关注按钮，请长按保存二维码图片，在微信中扫码关注公众号。
+      <view tc f14 c-999 mt20px>
+        接收预约提醒、消费通知等
       </view>
     </view>
   </view>
+
+  <!-- === 备用方案：web-view 方式 === -->
+  <!-- #ifdef MP-WEIXIN -->
+  <!-- <web-view :src="officialAccountUrl" /> -->
+  <!-- #endif -->
 </template>
 
 <style lang='scss' scoped></style>
